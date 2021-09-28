743a4fc598c16dc6d533f8a8a079198cd628f0e2
leguass7 <leandro.sbrissa@hotmail.com>
2021-09-28 19:34:17 -0300

    refactor: h1 para título da página

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,10 +5,7 @@ const PageTitleContainer = styled.div`
   display: block;
   padding: ${({ theme }) => theme.spacing.l}px 0;
   color: ${({ theme }) => theme.colors.primary};
-  h1,
-  h2,
-  h3,
-  h4 {
+  h1 {
     padding: 0;
     margin: 0 auto;
     font-size: 20px;
