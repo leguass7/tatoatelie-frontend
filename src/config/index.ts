06a045757b95d2df0322c3e31ab4abd5ab653df8
leguass7 <leandro.sbrissa@hotmail.com>
2021-11-07 17:32:11 -0300

    feat(helpers): adicionado isMounted e makeArray

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,5 @@ const host = dev ? 'localhost:3000' : vhost || ''
 export const imageBase = dev ? `http://${host}` : `${host ? `https://${host}` : ''}`
 
 export const imageBaseApi = 'https://server.tatoatelie.com.br/api'
+
+export const saltKey = process.env.SECRET
