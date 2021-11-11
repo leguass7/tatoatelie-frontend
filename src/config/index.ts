75de1fc91c124ac03f1cf8605ef9a16297194f35
leguass7 <leandro.sbrissa@hotmail.com>
2021-11-10 22:34:19 -0300

    feat: manifest

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ export const dev = process.env.NODE_ENV !== 'production'
 
 const virtualHost = `${process.env.NEXT_PUBLIC_VIRTUAL_HOST}` || ''
 const vhost = typeof virtualHost === 'undefined' || virtualHost === 'undefined' ? '' : virtualHost
-const host = dev ? 'localhost:3000' : vhost || ''
+export const host = dev ? 'localhost:3000' : vhost || ''
 
 export const imageBase = dev ? `http://${host}` : `${host ? `https://${host}` : ''}`
 
